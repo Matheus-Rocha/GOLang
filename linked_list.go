4314581dc4fcb706c51fd90e1d6cf67f218a3473
Matheus Rocha <49811477+Matheus-Rocha@users.noreply.github.com>
2021-08-30 08:15:27 -0300

    GO linked list data struct

--- linked_list.go
+++ linked_list.go
@@ -1,107 +1,126 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
-type Lista struct {
-	qtd    int
-	inicio *Registro
+type List struct {
+	qtd    int64
+	init *Register
 }
 
-type Registro struct {
-	valor   int
-	proximo *Registro
+type Register struct {
+	value   int64
+	next *Register
 }
 
 func main() {
 
-	fmt.Println(("Startando o programa de Grafos..."))
+	fmt.Println(("Starting linked list..."))
 
-	lista1 := Lista{
+	list := List{
 		qtd:    1,
-		inicio: &Registro{},
+		init: &Register{},
 	}
+	scanner := bufio.NewScanner(os.Stdin)
 
-	lista1.InsereOrdenado(4)
-	lista1.InsereOrdenado(1)
-	lista1.InsereOrdenado(2)
-	lista1.InsereOrdenado(6)
-	lista1.InsereOrdenado(5)
-	lista1.InsereOrdenado(10)
-	lista1.MostrarLista()
+	for {
+		fmt.Println("1 - Add item in list ")
+		fmt.Println("1 - Show the list ")
+		fmt.Println("9 - leave ")
+		scanner.Scan()
+		option := scanner.Text()
+		switch option {
+		case "1":
+			fmt.Println("Item: ")
+			scanner.Scan()
+			item, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+			list.OrdelyInsert(item)
+		case "2":
+			list.ShowList()
+			break
+		case "9":
+			os.Exit(99)
+		default:
+			fmt.Println("Invalid input")
+		}
+
+	}
 
 }
 
-func (l *Lista) InsereOrdenado(valor int) {
+func (l *List) OrdelyInsert(value int64) {
 
-	if (&Lista{}) == l {
-		fmt.Println("Lista vazia")
+	if (&List{}) == l {
+		fmt.Println("Empty list")
 	}
 
-	var aux *Registro = &Registro{}
-	var ant *Registro = &Registro{}
-	var novo *Registro = &Registro{}
+	var aux *Register = &Register{}
+	var ant *Register = &Register{}
+	var new *Register = &Register{}
 
-	novo.valor = valor
+	new.value = value
 
-	if l.inicio.valor == 0 {
-		l.inicio.valor = novo.valor
+	if l.init.value == 0 {
+		l.init.value = new.value
 
 	} else {
 
-		aux = l.inicio
+		aux = l.init
 		is_insert := false
 		for {
 			if (aux == nil || is_insert == true) {
 				break
 			} else {
 
-				if novo.valor == aux.valor {
-					fmt.Println("Valor repetido")
+				if new.value == aux.value {
+					fmt.Println("Value already in list")
 				}
 
-				if aux.valor < novo.valor{
+				if aux.value < new.value{
 					ant = aux
-					aux = aux.proximo
+					aux = aux.next
 				} else {
 
 
-					if ant.valor == 0 {
-						l.inicio = novo
+					if ant.value == 0 {
+						l.init = new
 					} else {
-						ant.proximo = novo
+						ant.next = new
 					}
-					novo.proximo = aux
+					new.next = aux
 					is_insert = true
 				}
 			}
 		}
 		if is_insert == false {
-			ant.proximo = novo
+			ant.next = new
 			is_insert = true
 		}
 	}
 	l.qtd++
 }
 
-func (l *Lista) MostrarLista() {
+func (l *List) ShowList() {
 
-	var aux *Registro
-	if (&Lista{}) == l {
-		fmt.Println("Lista não existe")
+	var aux *Register
+	if (&List{}) == l {
+		fmt.Println("List does not exist")
 	}
-	if l.inicio == nil {
-		fmt.Println("Lista vazia.")
+	if l.init == nil {
+		fmt.Println("Empty list")
 	}
 
-	aux = l.inicio
-	fmt.Println("Mostrando a lista...")
+	aux = l.init
+	fmt.Println("Printing the list...")
 
-	for i := 0; i < l.qtd; i++ {
+	for i := int64(0); i < l.qtd; i++ {
 		if aux != nil {
-			fmt.Printf("%v \n", *&aux.valor)
-			aux = aux.proximo
+			fmt.Printf("%v \n", *&aux.value)
+			aux = aux.next
 		}
 	}
 }
